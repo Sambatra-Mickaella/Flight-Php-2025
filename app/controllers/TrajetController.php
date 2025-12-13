e5fab82d6cff30d18bd46420cb76d2a73a59e373
tsuandriamarondraibe-collab <tsuandriamarondraibe@gmail.com>
2025-12-13 08:06:49 +0300

    trajet controller

---
+++ app/controllers/TrajetController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace app\controllers;
+
+use flight\Engine;
+use app\models\Trajets;
+use Flight;
+
+class TrajetController {
+
+    protected Engine $app;
+
+    public function __construct($app) {
+        $this->app = $app;
+    }
+
+    public function detail($id) {
+        $parcoursModel = new Trajets(Flight::db());
+
+        $parcours = $parcoursModel->getParcours($id);
+        $trajets  = $parcoursModel->getTrajetsByParcours($id);
+        $stats    = $parcoursModel->getStatParcours($id);
+
+        $this->app->render('detail', [
+            'parcours' => $parcours,
+            'trajets'  => $trajets,
+            'stats'    => $stats
+        ]);
+    }
+}
